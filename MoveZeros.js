e18319188aa8c14319286ffd2fe1c694889ca8a7
surya-karthik <78813375+surya-karthik@users.noreply.github.com>
2023-06-27 18:37:57 +0530

    Add files via upload

---
+++ MoveZeros.js
@@ -0,0 +1,21 @@
+function moveZeros(nums) {
+
+    let insertPos = 0;
+
+    for (i=0; i<nums.length; i++){
+        if (nums[i] !== 0) {
+            nums[insertPos] = nums[i];
+            insertPos++;
+
+        }
+    }
+
+    while(insertPos<nums.length){
+        nums[insertPos]=0;
+        insertPos++;
+    }
+}
+
+let nums = [0, 1, 0, 3, 12];
+moveZeroes(nums);
+console.log(nums);
